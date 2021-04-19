0804c36c0edc471e90f0c7f6621e05aa6184bb97
oth93 <abdulrahman.othman@ges.thm.de>
2021-04-19 11:22:25 +0200

    Update

--- Lambda/src/Obaide/Application.java
+++ Lambda/src/Obaide/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		myRunable m = new myRunable();
 		m.run();
+		
 		Runnable myRun1 =()->{
 			System.out.println("Welcome with Lambda");
 		};
